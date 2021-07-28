c0ce34c21bb6449f3bb63d5908f5cef5d44c84c8
Collin Marando <collin.marando@bellmedia.ca>
2021-07-28 14:50:51 -0400

    Hash Tables

--- clox/value.c
+++ clox/value.c
@@ -40,14 +40,9 @@ bool valuesEqual(Value a, Value b) {
   if (a.type != b.type) return false;
   switch (a.type) {
     case VAL_BOOL:   return a.as.boolean == b.as.boolean;
-    case VAL_NIL:    return true;
     case VAL_NUMBER: return a.as.number == b.as.number;
-    case VAL_OBJ: {
-        ObjString* aString = AS_STRING(a);
-        ObjString* bString = AS_STRING(b);
-        return aString->length == bString->length &&
-            memcmp(aString->chars, bString->chars, aString->length) == 0;
-    }
+    case VAL_OBJ:    return a.as.obj == b.as.obj;
+    case VAL_NIL:    return true;
     default:         return false; // Unreachable.
   }
 }
